40d281dbda99d3df4a39250087f3592d8c4363f7
caiorolim <caiorolim.squadra@parceiro.afya.com.br>
2024-09-11 08:11:21 -0300

    Aula 9: Listando pokemons capturados

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -6,6 +6,12 @@ const router = express.Router();
 
 router.get('/', (_req , res)=>{
     Pokemon.find().then((pokemons)=>{
+        pokemons.forEach(pokemon =>{
+            const dataIso = pokemon._id.getTimestamp().toISOString();
+            const datainfo = dataIso.split('T')[0];
+            const dataCaptura= datainfo.split('-');
+            pokemon.capturadoEm =  `${dataCaptura[2]}/${dataCaptura[1]}/${dataCaptura[0]}`
+        })
         res.render('paginas/pokemons/index', {
             pokemons,
         });
