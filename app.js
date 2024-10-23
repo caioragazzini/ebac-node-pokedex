cfb6e4f12e93e07c43461c5b8feffdf960aeaba6
caiorolim <caiorolim.squadra@parceiro.afya.com.br>
2024-10-23 20:53:40 -0300

    Modulo 10 - Aula 3: Implementando login por email e senha (passport)

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 require('dotenv').config();
 
 const express = require('express');
+
+const passport = require('passport');
+const session = require('express-session');
 const createError = require('http-errors');
+
 const {connect} = require('./models');
+require('./routes/auth/');
 const pokemonRouter = require('./routes/pokemons');
 const batalhaRouter = require('./routes/batalha');
 const apiRouter =require('./routes/api')
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 
-const app = express();
 const porta = 3000;
+const app = express();
+
+//configurando a autenticação
+app.use(session({
+    secret:process.env.SESSION_SECRET,
+    resave : false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // configurando ejs
 app.set('views', path.join(__dirname, 'views') );
---
+++ routes/auth/local.js
@@ -0,0 +1,43 @@
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const {Usuario} = require('../../models');
+const bcrypt = require('bcrypt');
+
+passport.use(new localStrategy({
+    usernameField: 'email'
+}, async(nomeDeUsuario, senha, done)=>{
+    try{
+        const usuario = await Usuario.findOne({email: nomeDeUsuario });
+        if(!usuario){
+            return done(null,false);
+        }
+
+        const senhaCorreta = await bcrypt.compare(senha, usuario.senha);       
+        if(senhaCorreta){
+            return done(null, usuario);
+        } else{
+            return done(null, false);            
+        }
+
+    } catch(e){
+        done(e, false);
+    }
+}));
+
+//salva na sessão o usuario
+passport.serializeUser((usuario, done)=>{
+    done(null, usuario._id);
+});
+
+//recuperando da sessão o usuario
+passport.deserializeUser( async(id, done)=>{
+    let err, usuario;
+    try{
+        usuario = await User.findById(id);
+
+    } catch(err){
+        err = err;
+    }
+    done(err, usuario);
+
+});
